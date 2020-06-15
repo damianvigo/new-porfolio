60d5f83b7c51248c017039a6b106a52da3c5aea6
Damian <damiandanielvigo@gmail.com>
2020-06-15 14:38:40 -0300

    grid-fix

--- js/index.js
+++ js/index.js
@@ -11,3 +11,23 @@ d.addEventListener('DOMContentLoaded', (e) => {
   scrollTopButton('.scroll-top-btn');
   darkMode('#switch');
 });
+
+const overlay = document.getElementById('overlay');
+d.querySelectorAll('.proyects__images__img').forEach((el) => {
+  el.addEventListener('click', () => {
+    const rute = el.getAttribute('src');
+    const description = el.parentNode.dataset.description;
+    overlay.classList.add('active');
+    d.querySelector('.overlay__img').src = rute;
+    d.querySelector('.overlay__description').innerHTML = description;
+  });
+});
+
+d.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+  overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', (e) => {
+  // overlay.classList.remove('active');
+  e.target.id === 'overlay' ? overlay.classList.remove('active') : '';
+});
